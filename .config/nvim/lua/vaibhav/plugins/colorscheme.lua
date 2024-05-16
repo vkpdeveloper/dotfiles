7b2c1e81a1a90a46e7a2aa0e3852a726f967512e
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-05-16 16:45:58 +0530

    alacritty opacity changes and removed opacity for code from picom

--- .config/nvim/lua/vaibhav/plugins/colorscheme.lua
+++ .config/nvim/lua/vaibhav/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-    "projekt0n/github-nvim-theme",
-    lazy = false,
-    name = "github-dark",
-    priority = 1000,
-    config = function()
-        require('github-theme').setup({})
-        vim.cmd([[colorscheme github_dark_colorblind]])
-    end
+	"projekt0n/github-nvim-theme",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("github-theme").setup({})
+		vim.cmd([[colorscheme github_dark_colorblind]])
+	end,
 }
