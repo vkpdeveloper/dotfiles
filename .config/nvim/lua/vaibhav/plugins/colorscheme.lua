8b391818a3eaff175e5c1b0c8b10808ad40043f0
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-06-28 18:48:40 +0530

    added new theme *kanagawa* and also made changes for alacritty and lualine

--- .config/nvim/lua/vaibhav/plugins/colorscheme.lua
+++ .config/nvim/lua/vaibhav/plugins/colorscheme.lua
@@ -1,9 +1,44 @@
 return {
-	"projekt0n/github-nvim-theme",
+	"rebelot/kanagawa.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("github-theme").setup({})
-		vim.cmd([[colorscheme github_dark_colorblind]])
+		require("kanagawa").setup({
+			compile = false, -- enable compiling the colorscheme
+			undercurl = false, -- enable undercurls
+			commentStyle = {
+				italic = false,
+			},
+			functionStyle = {
+				italic = false,
+			},
+			keywordStyle = {
+				italic = false,
+			},
+			statementStyle = {
+				italic = false,
+			},
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			colors = {
+				palette = {},
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+			overrides = function(colors)
+				local theme = colors.theme
+				return {}
+			end,
+			theme = "dragon",
+		})
+		require("kanagawa").load("dragon")
+		-- vim.cmd([[colorscheme kanagawa-dragon]])
 	end,
 }
