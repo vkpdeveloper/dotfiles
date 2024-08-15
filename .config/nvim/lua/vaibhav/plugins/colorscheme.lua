e4306ab863ced4f1e66fdb53b0f558eaf8b48bd1
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-08-15 22:35:37 +0530

    custom cappuccin setup

--- .config/nvim/lua/vaibhav/plugins/colorscheme.lua
+++ .config/nvim/lua/vaibhav/plugins/colorscheme.lua
@@ -1,50 +1,116 @@
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("kanagawa").setup({
+-- 			compile = false, -- enable compiling the colorscheme
+-- 			undercurl = false, -- enable undercurls
+-- 			commentStyle = {
+-- 				italic = false,
+-- 			},
+-- 			functionStyle = {
+-- 				italic = false,
+-- 			},
+-- 			keywordStyle = {
+-- 				italic = false,
+-- 			},
+-- 			statementStyle = {
+-- 				bold = false,
+-- 				italic = false,
+-- 			},
+-- 			typeStyle = {},
+-- 			transparent = false, -- do not set background color
+-- 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+-- 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+-- 			colors = {
+-- 				palette = {},
+-- 				theme = {
+-- 					all = {
+-- 						ui = {
+-- 							bg_gutter = "none",
+-- 						},
+-- 					},
+-- 				},
+-- 			},
+-- 			overrides = function(colors)
+-- 				local theme = colors.theme
+-- 				return {
+-- 					["@variable.builtin"] = {
+-- 						bg = "none",
+-- 						italic = false,
+-- 					},
+-- 				}
+-- 			end,
+-- 			theme = "dragon",
+-- 		})
+-- 		require("kanagawa").load("dragon")
+-- 		-- vim.cmd([[colorscheme kanagawa-dragon]])
+-- 	end,
+-- }
+
+-- return {
+-- 	"nyoom-engineering/oxocarbon.nvim",
+-- 	config = function()
+-- 		vim.opt.background = "dark"
+-- 		vim.cmd("colorscheme oxocarbon")
+-- 	end,
+-- }
+
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
+	"catppuccin/nvim",
 	priority = 1000,
+	name = "catppuccin",
 	config = function()
-		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = false, -- enable undercurls
-			commentStyle = {
-				italic = false,
+		require("catppuccin").setup({
+			flavour = "mocha",
+			no_italic = true,
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
 			},
-			functionStyle = {
-				italic = false,
-			},
-			keywordStyle = {
-				italic = false,
-			},
-			statementStyle = {
-				bold = false,
-				italic = false,
-			},
-			typeStyle = {},
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = {
-				palette = {},
-				theme = {
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
 				},
 			},
-			overrides = function(colors)
-				local theme = colors.theme
-				return {
-					["@variable.builtin"] = {
-						bg = "none",
-						italic = false,
-					},
-				}
-			end,
-			theme = "dragon",
+			-- integrations = {
+			-- 	telescope = {
+			-- 		enabled = true,
+			-- 		style = "nvchad",
+			-- 	},
+			-- },
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
 		})
-		require("kanagawa").load("dragon")
-		-- vim.cmd([[colorscheme kanagawa-dragon]])
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
