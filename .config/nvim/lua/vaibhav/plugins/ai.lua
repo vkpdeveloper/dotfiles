5338c636df4a125621a54bd2336427369b51e33a
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-11-10 11:45:45 +0530

    feat(yabai+skhd): Added yabai and skhd config

--- .config/nvim/lua/vaibhav/plugins/ai.lua
+++ .config/nvim/lua/vaibhav/plugins/ai.lua
@@ -1,12 +1,14 @@
 return {
-	"supermaven-inc/supermaven-nvim",
-	config = function()
-		require("supermaven-nvim").setup({
-			keymaps = {
-				accept_suggestion = "<C-g>",
-				clear_suggestion = "<C-]>",
-				accept_word = "<C-j>",
-			},
-		})
-	end,
+    "Exafunction/codeium.vim",
+    event = 'BufEnter',
+    config = function()
+        local keymap = vim.keymap
+
+        keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+            { expr = true, silent = true })
+        keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+            { expr = true, silent = true })
+        keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end,
 }
