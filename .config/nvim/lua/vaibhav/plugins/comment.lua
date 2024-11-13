2e0d7ccd7759ad771dd95739352fc1feb5a34e32
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-11-13 09:31:49 +0530

    added support for tsx commentstring

--- .config/nvim/lua/vaibhav/plugins/comment.lua
+++ .config/nvim/lua/vaibhav/plugins/comment.lua
@@ -1,9 +1,14 @@
 return {
     'numToStr/Comment.nvim',
-    opts = {},
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
     lazy = true,
     event = {
         'BufReadPre',
         'BufNewFile'
-    }
+    },
+    config = function(opts)
+        require('Comment').setup({
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        })
+    end
 }
