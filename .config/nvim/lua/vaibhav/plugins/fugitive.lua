c88fa4d34902e0de283332206e945e093a2d0e8e
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-11-18 11:41:21 +0530

    Removed quit on q shortcut from fugitive

--- .config/nvim/lua/vaibhav/plugins/fugitive.lua
+++ .config/nvim/lua/vaibhav/plugins/fugitive.lua
@@ -1,34 +1,32 @@
 return {
-	"tpope/vim-fugitive",
-	cmd = { "Git", "Gdiffsplit", "Gvdiffsplit" },
-	lazy = false,
-	config = function()
-		-- thanks to @ThePrimeagen
-		local Vaibhav_Fugitive = vim.api.nvim_create_augroup("vaibhav_fugitive", {})
-		vim.keymap.set("n", "<C-G>", "<Cmd>Git<Cr>")
+    "tpope/vim-fugitive",
+    cmd = { "Git", "Gdiffsplit", "Gvdiffsplit" },
+    lazy = false,
+    config = function()
+        -- thanks to @ThePrimeagen
+        local Vaibhav_Fugitive = vim.api.nvim_create_augroup("vaibhav_fugitive", {})
+        vim.keymap.set("n", "<C-G>", "<Cmd>Git<Cr>")
 
-		local autocmd = vim.api.nvim_create_autocmd
-		autocmd("BufWinEnter", {
-			group = Vaibhav_Fugitive,
-			pattern = "*",
-			callback = function()
-				if vim.bo.ft ~= "fugitive" then
-					return
-				end
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = Vaibhav_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
 
-				local bufnr = vim.api.nvim_get_current_buf()
-				local opts = { buffer = bufnr, remap = false }
-				vim.keymap.set("n", "<leader>p", function()
-					vim.cmd.Git("push")
-				end, opts)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git("push")
+                end, opts)
 
-				-- rebase always
-				vim.keymap.set("n", "<leader>P", function()
-					vim.cmd.Git({ "pull --rebase" })
-				end, opts)
-
-				vim.keymap.set("n", "q", vim.cmd.quit)
-			end,
-		})
-	end,
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({ "pull --rebase" })
+                end, opts)
+            end,
+        })
+    end,
 }
