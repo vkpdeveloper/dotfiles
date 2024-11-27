33f0e16f1696eda64a6cb581241b93ce4bf873ba
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-11-27 12:00:08 +0530

    Awesome plugin changes and lualine changes for better statusline

--- .config/nvim/lua/vaibhav/plugins/ai.lua
+++ .config/nvim/lua/vaibhav/plugins/ai.lua
@@ -1,14 +1,20 @@
 return {
-    "Exafunction/codeium.vim",
-    event = 'BufEnter',
-    config = function()
-        local keymap = vim.keymap
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+	config = function()
+		local keymap = vim.keymap
 
-        keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-        keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-            { expr = true, silent = true })
-        keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-            { expr = true, silent = true })
-        keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-    end,
+		keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		keymap.set("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		keymap.set("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+	end,
 }
--- .config/nvim/lua/vaibhav/plugins/lualine.lua
+++ .config/nvim/lua/vaibhav/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = "ayu_dark",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
@@ -19,18 +19,18 @@ return {
 				always_divide_middle = true,
 				globalstatus = false,
 				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
+					statusline = 100,
+					tabline = 100,
+					winbar = 100,
 				},
 			},
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "buffers" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "location", "tabs" },
-				lualine_z = { "hostname" },
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = { "searchcount", "filetype" },
+				lualine_z = { "location" },
 			},
 			inactive_sections = {
 				lualine_a = {},
