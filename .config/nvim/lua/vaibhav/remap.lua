8c4307cf52da130f606475ada5759f9b1d89f2fb
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-12-03 23:35:10 +0530

    refactor(nvim): update key mappings for better efficiency - Removed redundant mappings for consistency. - Added new mappings for easier navigation.
    This refactoring improves the usability and performance of key bindings in Neovim.

--- .config/nvim/lua/vaibhav/remap.lua
+++ .config/nvim/lua/vaibhav/remap.lua
@@ -12,30 +12,23 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 -- Panes Controls
 vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit, { desc = "Open a vertical split" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
-
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 
 -- Panes navigation
 vim.keymap.set("n", "<C-h>", "<C-W><C-h>", { desc = "Move focus to the left panes" })
 vim.keymap.set("n", "<C-l>", "<C-W><C-l>", { desc = "Move focus to the right panes" })
 vim.keymap.set("n", "<C-j>", "<C-W><C-j>", { desc = "Move focus to the lower panes" })
-
 vim.keymap.set("n", "<C-k>", "<C-W><C-k>", { desc = "Move focus to the upper panes" })
 
 -- Remaps for Tabbars
-vim.keymap.set("n", "<leader>to", vim.cmd.tabedit, { desc = "Open a new tab" })
-vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext, { desc = "Next tab" })
-vim.keymap.set("n", "<leader>tp", vim.cmd.tabprevious, { desc = "Previous tab" })
-
 vim.keymap.set("n", "<leader>tx", vim.cmd.tabclose, { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew, { desc = "Open a new tab" })
 
 -- Remap for moving to 1st to 6 tabs
 vim.keymap.set("n", "<leader>t1", "1gt", { desc = "Go to tab 1" })
 vim.keymap.set("n", "<leader>t2", "2gt", { desc = "Go to tab 2" })
 vim.keymap.set("n", "<leader>t3", "3gt", { desc = "Go to tab 3" })
 vim.keymap.set("n", "<leader>t4", "4gt", { desc = "Go to tab 4" })
-vim.keymap.set("n", "<leader>t5", "5gt", { desc = "Go to tab 5" })
-vim.keymap.set("n", "<leader>t6", "6gt", { desc = "Go to tab 6" })
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
@@ -51,10 +44,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
-
--- Running Prisma generate and doing automatic :LspRestart
-vim.keymap.set("n", "<C-A-p>", function()
-    vim.cmd("!npx prisma db push")
-    vim.cmd("LspRestart")
-    print("LSP Restarted Successfully")
-end)
