67419c380e02fae169f6d5b7ab25eb78c5f58703
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-12-21 19:37:33 +0530

    feat(ui): add dressing.nvim for enhanced input and select UI
    Added dressing.nvim plugin to improve the appearance of vim.ui.input and vim.ui.select interfaces. Also reformatted LSP configuration file for better readability with consistent indentation.
    
    Resolves #UI-enhancement

--- .config/nvim/lua/vaibhav/plugins/lsp.lua
+++ .config/nvim/lua/vaibhav/plugins/lsp.lua
@@ -1,144 +1,144 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	branch = "v3.x",
-	dependencies = {
-		{ "neovim/nvim-lspconfig" },
-		{
-			"williamboman/mason.nvim",
-			opts = {
-				automatic_installation = true,
-				ensure_installed = {
-					"js-debug-adapter",
-					"lua_ls",
-					"rust_analyzer",
-					"clangd",
-					"codelldb",
-					"gopls",
-					"docker_compose_language_service",
-					"dockerls",
-					"prismals",
-					"tailwindcss",
-					"eslint_d",
-					"prettierd",
-					"delve",
-					"black",
-					"isort",
-				},
-			},
-		},
-		{ "williamboman/mason-lspconfig.nvim" },
-	},
-	config = function()
-		local lsp = require("lsp-zero")
-		lsp.preset("recommended")
+    "VonHeikemen/lsp-zero.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    branch = "v3.x",
+    dependencies = {
+        { "neovim/nvim-lspconfig" },
+        {
+            "williamboman/mason.nvim",
+            opts = {
+                automatic_installation = true,
+                ensure_installed = {
+                    "js-debug-adapter",
+                    "lua_ls",
+                    "rust_analyzer",
+                    "clangd",
+                    "codelldb",
+                    "gopls",
+                    "docker_compose_language_service",
+                    "dockerls",
+                    "prismals",
+                    "tailwindcss",
+                    "eslint_d",
+                    "prettierd",
+                    "delve",
+                    "black",
+                    "isort",
+                },
+            },
+        },
+        { "williamboman/mason-lspconfig.nvim" },
+    },
+    config = function()
+        local lsp = require("lsp-zero")
+        lsp.preset("recommended")
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"ts_ls",
-				"rust_analyzer",
-				"clangd",
-				"golangci_lint_ls",
-				"gopls",
-				"docker_compose_language_service",
-				"dockerls",
-				"biome",
-				"prismals",
-				"tailwindcss",
-				"lua_ls",
-			},
-			handlers = {
-				lsp.default_setup,
-			},
-		})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "ts_ls",
+                "rust_analyzer",
+                "clangd",
+                "golangci_lint_ls",
+                "gopls",
+                "docker_compose_language_service",
+                "dockerls",
+                "biome",
+                "prismals",
+                "tailwindcss",
+                "lua_ls",
+            },
+            handlers = {
+                lsp.default_setup,
+            },
+        })
 
-		lsp.set_sign_icons({
-			error = "✘",
-			warn = "▲",
-			hint = "⚑",
-			info = "»",
-		})
+        lsp.set_sign_icons({
+            error = "✘",
+            warn = "▲",
+            hint = "⚑",
+            info = "»",
+        })
 
-		lsp.configure("lua_ls", {
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim", "require" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
+        lsp.configure("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        globals = { "vim", "require" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
+        })
 
-		require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-		lsp.setup_servers({
-			"ts_ls",
-			"rust_analyzer",
-			"lua_ls",
-			"tailwindcss",
-			"gopls",
-			"dockerls",
-			"docker_compose_language_service",
-			"prismals",
-			"clangd",
-		})
+        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+        lsp.setup_servers({
+            "ts_ls",
+            "rust_analyzer",
+            "lua_ls",
+            "tailwindcss",
+            "gopls",
+            "dockerls",
+            "docker_compose_language_service",
+            "prismals",
+            "clangd",
+        })
 
-		lsp.on_attach(function(client, bufnr)
-			lsp.default_keymaps({ buffer = bufnr })
-			lsp.highlight_symbol(client, bufnr)
-			local opts = { buffer = bufnr, remap = false }
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>ws", function()
-				vim.lsp.buf.workspace_symbol()
-			end, opts)
-			vim.keymap.set("n", "<leader>vd", function()
-				vim.diagnostic.open_float()
-			end, opts)
-			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.goto_next()
-			end, opts)
-			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.goto_prev()
-			end, opts)
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>rf", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-		end)
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+            lsp.highlight_symbol(client, bufnr)
+            local opts = { buffer = bufnr, remap = false }
+            vim.keymap.set("n", "gd", function()
+                vim.lsp.buf.definition()
+            end, opts)
+            vim.keymap.set("n", "K", function()
+                vim.lsp.buf.hover()
+            end, opts)
+            vim.keymap.set("n", "<leader>ws", function()
+                vim.lsp.buf.workspace_symbol()
+            end, opts)
+            vim.keymap.set("n", "<leader>vd", function()
+                vim.diagnostic.open_float()
+            end, opts)
+            vim.keymap.set("n", "[d", function()
+                vim.diagnostic.goto_next()
+            end, opts)
+            vim.keymap.set("n", "]d", function()
+                vim.diagnostic.goto_prev()
+            end, opts)
+            vim.keymap.set("n", "<leader>ca", function()
+                vim.lsp.buf.code_action()
+            end, opts)
+            vim.keymap.set("n", "<leader>rf", function()
+                vim.lsp.buf.references()
+            end, opts)
+            vim.keymap.set("n", "<leader>rn", function()
+                vim.lsp.buf.rename()
+            end, opts)
+            vim.keymap.set("i", "<C-h>", function()
+                vim.lsp.buf.signature_help()
+            end, opts)
+        end)
 
-		-- Setup dockerls
-		require("lspconfig").dockerls.setup({
-			on_attach = lsp.on_attach,
-			capabilities = lsp.capabilities,
-			filetypes = { "Dockerfile", "dockerfile" },
-			cmd = { "docker-langserver", "--stdio" },
-		})
+        -- Setup dockerls
+        require("lspconfig").dockerls.setup({
+            on_attach = lsp.on_attach,
+            capabilities = lsp.capabilities,
+            filetypes = { "Dockerfile", "dockerfile" },
+            cmd = { "docker-langserver", "--stdio" },
+        })
 
-		lsp.setup()
-		vim.diagnostic.config({
-			virtual_text = true,
-		})
-	end,
+        lsp.setup()
+        vim.diagnostic.config({
+            virtual_text = true,
+        })
+    end,
 }
