ee8c7e5c2481b48cb9d25f97d71acc210c188fc5
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-12-29 19:08:16 +0530

    feat(scripts): add optimus-manager check in nvidia script
    Enhance nvidia GPU script by adding a check for optimus-manager installation. Display "GPU: No" if optimus-manager is not installed, ensuring proper GPU status reporting. Also includes minor wallpaper order adjustment in fehbg.

--- .config/polybar/scripts/nvidia-semi-gpu.sh
+++ .config/polybar/scripts/nvidia-semi-gpu.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "GPU",""$1"","%"}'
+# check if optimus-manager is installed
+if ! command -v optimus-manager &> /dev/null
+then
+    echo "GPU: No"
+    exit
+else
+    nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "GPU",""$1"","%"}'
+fi
+
+
