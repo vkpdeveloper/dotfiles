01a468596291ed275f0ca7545d9b91330fa4725d
vkpdeveloper <vaibhavpathak9984@gmail.com>
2025-01-01 13:32:40 +0530

    feat(completion): migrate from nvim-cmp to blink.cmp
    Replaced the traditional nvim-cmp setup with blink.cmp for enhanced completion
    capabilities. This change includes removal of previous dependencies like
    LuaSnip and lspkind while adding blink.cmp configurations for improved LSP
    integration and cleaner completion interface.
    
    BREAKING CHANGE: Complete overhaul of completion system from nvim-cmp to blink.cmp

--- .config/nvim/lua/vaibhav/plugins/lsp.lua
+++ .config/nvim/lua/vaibhav/plugins/lsp.lua
@@ -1,174 +1,172 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    branch = "v3.x",
-    dependencies = {
-        { "neovim/nvim-lspconfig" },
-        {
-            "williamboman/mason.nvim",
-            opts = {
-                automatic_installation = true,
-                ensure_installed = {
-                    "js-debug-adapter",
-                    "lua_ls",
-                    "rust_analyzer",
-                    "clangd",
-                    "codelldb",
-                    "gopls",
-                    "dockerls",
-                    "prismals",
-                    "eslint_d",
-                    "prettierd",
-                    "delve",
-                    "black",
-                    "isort",
-                },
-            },
-        },
-        { "williamboman/mason-lspconfig.nvim" },
-    },
-    config = function()
-        local lsp = require("lsp-zero")
-        lsp.preset("recommended")
+	"VonHeikemen/lsp-zero.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	branch = "v3.x",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+		{ "saghen/blink.cmp" },
+		{
+			"williamboman/mason.nvim",
+			opts = {
+				automatic_installation = true,
+				ensure_installed = {
+					"js-debug-adapter",
+					"lua_ls",
+					"rust_analyzer",
+					"clangd",
+					"codelldb",
+					"gopls",
+					"dockerls",
+					"prismals",
+					"eslint_d",
+					"prettierd",
+					"delve",
+					"black",
+					"isort",
+				},
+			},
+		},
+		{ "williamboman/mason-lspconfig.nvim" },
+	},
+	config = function()
+		local lsp = require("lsp-zero")
+		lsp.preset("recommended")
 
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "ts_ls",
-                "rust_analyzer",
-                "clangd",
-                "golangci_lint_ls",
-                "gopls",
-                "dockerls",
-                "biome",
-                "prismals",
-                "lua_ls",
-            },
-            handlers = {
-                lsp.default_setup,
-            },
-            automatic_installation = true
-        })
+		local lspconfig = require("lspconfig")
+		local blinkCapabilities = require("blink.cmp").get_lsp_capabilities()
 
-        local signs = {
-            { name = "DiagnosticSignError", text = "✘" },
-            { name = "DiagnosticSignWarn", text = "▲" },
-            { name = "DiagnosticSignHint", text = "⚑" },
-            { name = "DiagnosticSignInfo", text = "" },
-        }
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"ts_ls",
+				"rust_analyzer",
+				"clangd",
+				"golangci_lint_ls",
+				"gopls",
+				"dockerls",
+				"biome",
+				"prismals",
+				"lua_ls",
+			},
+			handlers = {
+				lsp.default_setup,
+			},
+			automatic_installation = true,
+		})
 
-        lsp.set_sign_icons({
-            error = "✘",
-            warn = "▲",
-            hint = "⚑",
-            info = "",
-        })
+		local signs = {
+			{ name = "DiagnosticSignError", text = "✘" },
+			{ name = "DiagnosticSignWarn", text = "▲" },
+			{ name = "DiagnosticSignHint", text = "⚑" },
+			{ name = "DiagnosticSignInfo", text = "" },
+		}
 
-        lsp.configure("lua_ls", {
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = "LuaJIT",
-                    },
-                    diagnostics = {
-                        globals = { "vim", "require" },
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                    },
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
-        })
+		lsp.set_sign_icons({
+			error = "✘",
+			warn = "▲",
+			hint = "⚑",
+			info = "",
+		})
 
-        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-        lsp.setup_servers({
-            "ts_ls",
-            "rust_analyzer",
-            "lua_ls",
-            "tailwindcss",
-            "gopls",
-            "dockerls",
-            "docker_compose_language_service",
-            "prismals",
-            "clangd",
-        })
+		lsp.configure("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = { "vim", "require" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+		})
 
-        lsp.on_attach(function(client, bufnr)
-            lsp.default_keymaps({ buffer = bufnr })
-            lsp.highlight_symbol(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end, opts)
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end, opts)
-            vim.keymap.set("n", "<leader>ws", function()
-                vim.lsp.buf.workspace_symbol()
-            end, opts)
-            vim.keymap.set("n", "<leader>vd", function()
-                vim.diagnostic.open_float()
-            end, opts)
-            vim.keymap.set("n", "[d", function()
-                vim.diagnostic.goto_next()
-            end, opts)
-            vim.keymap.set("n", "]d", function()
-                vim.diagnostic.goto_prev()
-            end, opts)
-            vim.keymap.set("n", "<leader>ca", function()
-                vim.lsp.buf.code_action()
-            end, opts)
-            vim.keymap.set("n", "<leader>rf", function()
-                vim.lsp.buf.references()
-            end, opts)
-            vim.keymap.set("n", "<leader>rn", function()
-                vim.lsp.buf.rename()
-            end, opts)
-            vim.keymap.set("i", "<C-h>", function()
-                vim.lsp.buf.signature_help()
-            end, opts)
-            vim.keymap.set("n", "<leader>qf", "<CR><Cmd>:cw<CR>", opts)
-        end)
+		lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+		lsp.setup_servers({
+			"ts_ls",
+			"rust_analyzer",
+			"lua_ls",
+			"tailwindcss",
+			"gopls",
+			"dockerls",
+			"docker_compose_language_service",
+			"prismals",
+			"clangd",
+		}, {
+			capabilities = blinkCapabilities,
+		})
 
-        -- Setup dockerls
-        require("lspconfig").dockerls.setup({
-            on_attach = lsp.on_attach,
-            capabilities = lsp.capabilities,
-            filetypes = { "Dockerfile", "dockerfile" },
-            cmd = { "docker-langserver", "--stdio" },
-        })
+		lsp.on_attach(function(client, bufnr)
+			lsp.default_keymaps({ buffer = bufnr })
+			lsp.highlight_symbol(client, bufnr)
+			local opts = { buffer = bufnr, remap = false }
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "<leader>ws", function()
+				vim.lsp.buf.workspace_symbol()
+			end, opts)
+			vim.keymap.set("n", "<leader>vd", function()
+				vim.diagnostic.open_float()
+			end, opts)
+			vim.keymap.set("n", "[d", function()
+				vim.diagnostic.goto_next()
+			end, opts)
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.goto_prev()
+			end, opts)
+			vim.keymap.set("n", "<leader>ca", function()
+				vim.lsp.buf.code_action()
+			end, opts)
+			vim.keymap.set("n", "<leader>rf", function()
+				vim.lsp.buf.references()
+			end, opts)
+			vim.keymap.set("n", "<leader>rn", function()
+				vim.lsp.buf.rename()
+			end, opts)
+			vim.keymap.set("i", "<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end, opts)
+			vim.keymap.set("n", "<leader>qf", "<CR><Cmd>:cw<CR>", opts)
+		end)
 
-        lsp.setup()
-        vim.diagnostic.config({
-            virtual_text = true,
-            signs = {
-                active = signs,
-            },
-            update_in_insert = true,
-            underline = true,
-            severity_sort = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = true,
-                header = "",
-                prefix = "",
-            },
-        })
+		lsp.setup()
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = {
+				active = signs,
+			},
+			update_in_insert = true,
+			underline = true,
+			severity_sort = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = true,
+				header = "",
+				prefix = "",
+			},
+		})
 
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-            border = "rounded",
-            width = 60,
-        })
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = "rounded",
+			width = 60,
+		})
 
-        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-            border = "rounded",
-            width = 60,
-            close_events = { "CursorMoved", "BufHidden", "InsertCharPre" },
-        })
-    end,
+		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+			border = "rounded",
+			width = 60,
+			close_events = { "CursorMoved", "BufHidden", "InsertCharPre" },
+		})
+	end,
 }
