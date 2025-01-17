b9e2a0686db1d3cdf5b359663c011c3e74017f73
vkpdeveloper <vaibhavpathak9984@gmail.com>
2025-01-17 12:46:46 +0530

    feat(completion): add lsp signature help to nvim-cmp
    Adds support for LSP signature help in the completion menu by including "cmp-nvim-lsp-signature-help" plugin. This enhances the autocompletion experience by showing function signatures while typing.
    
    Dependencies: cmp-nvim-lsp-signature-help

--- .config/nvim/lua/vaibhav/plugins/completion.lua
+++ .config/nvim/lua/vaibhav/plugins/completion.lua
@@ -1,56 +1,58 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"saadparwaiz1/cmp_luasnip",
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			build = "make install_jsregexp",
-		},
-		"onsails/lspkind.nvim",
-	},
-	event = "InsertEnter",
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
+        "saadparwaiz1/cmp_luasnip",
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            build = "make install_jsregexp",
+        },
+        "onsails/lspkind.nvim",
+    },
+    event = "InsertEnter",
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
 
-		cmp.setup({
-			mapping = cmp.mapping.preset.insert({
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-			}),
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            }),
+            completion = {
+                completeopt = "menu,menuone,preview,noselect",
+            },
+            snippet = { -- configure how nvim-cmp interacts with snippet engine
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
 
-			-- sources for autocompletion
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-				{ name = "supermaven" },
-			}),
+            -- sources for autocompletion
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "cmp_nvim_lsp_signature_help" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "supermaven" },
+            }),
 
-			-- configure lspkind for vs-code like pictograms in completion menu
-			formatting = {
-				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-		})
-	end,
+            -- configure lspkind for vs-code like pictograms in completion menu
+            formatting = {
+                format = lspkind.cmp_format({
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                }),
+            },
+        })
+    end,
 }
 
 -- return {
