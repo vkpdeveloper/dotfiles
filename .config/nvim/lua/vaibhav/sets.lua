7bfa72cec57cb9ef29dfdf8b1e1c5d6731931ba6
vkpdeveloper <vaibhavpathak9984@gmail.com>
2025-02-18 09:35:23 +0530

    feat(tmux,nvim): Enhance clipboard and system integration
    - Updated `.tmux.conf` to enable clipboard and passthrough
    - Modified Neovim clipboard configuration to support OSC 52 and system clipboard
    - Added custom paste function for improved clipboard handling
    - Improved clipboard settings to work with unnamed and unnamedplus registers

--- .config/nvim/lua/vaibhav/sets.lua
+++ .config/nvim/lua/vaibhav/sets.lua
@@ -98,14 +98,23 @@ vim.opt.title = true
 
 vim.opt.spell = false
 
+local function paste()
+	return {
+		vim.fn.split(vim.fn.getreg(""), "\n"),
+		vim.fn.getregtype(""),
+	}
+end
+
 vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy '+',
-        ['*'] = require('vim.ui.clipboard.osc52').copy '*',
-    },
-    paste = {
-        ['+'] = require('vim.ui.clipboard.osc52').paste '+',
-        ['*'] = require('vim.ui.clipboard.osc52').paste '*',
-    },
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = paste,
+		["*"] = paste,
+	},
 }
+
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
