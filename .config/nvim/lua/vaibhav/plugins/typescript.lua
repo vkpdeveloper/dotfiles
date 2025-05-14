ab5f70f0c7c4dfac10a3c9fe23b90ff35b1d5b93
Vaibhav <vkpdeveloper>
2025-05-14 19:01:13 +0530

    great job

--- .config/nvim/lua/vaibhav/plugins/typescript.lua
+++ .config/nvim/lua/vaibhav/plugins/typescript.lua
@@ -1,24 +1,25 @@
 return {
-	"yioneko/nvim-vtsls",
-	dependencies = { "neovim/nvim-lspconfig" },
-	config = function()
-		require("lspconfig").vtsls.setup({
-			-- VTSLS is specifically designed to be lighter than standard tsserver
-			cmd = { "vtsls", "--stdio" },
-			filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-			root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "package.json", ".git"),
-			init_options = {
-				-- Disable non-essential features
-				hostInfo = "neovim",
-				disableAutomaticTypingAcquisition = true,
-				maxTsServerMemory = 4096,
-				tsserver = {
-					path = "", -- Let it find from node_modules
-					logDirectory = "/tmp/vtsls-logs/",
-					logVerbosity = "off",
-					trace = false,
-				},
-			},
-		})
-	end,
+    "yioneko/nvim-vtsls",
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+        require("lspconfig").vtsls.setup({
+            cmd = { "vtsls", "--stdio" },
+            filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+            root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "package.json", ".git"),
+            init_options = {
+                hostInfo = "neovim",
+                disableAutomaticTypingAcquisition = true,
+                maxTsServerMemory = 2048,
+                tsserver = {
+                    path = "",
+                    logDirectory = "/tmp/vtsls-logs/",
+                    logVerbosity = "off",
+                    trace = false,
+                },
+            },
+        })
+
+        vim.keymap.set('n', '<leader>mi', '<cmd>VtsExec add_missing_imports<CR>')
+        vim.keymap.set('n', '<leader>oi', '<cmd>VtsExec organize_imports<CR>')
+    end,
 }
